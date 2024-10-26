2ccb6299f53253c5d7fb0644ac7f077bde9a5852
lanor <giuseppe.trieste.97@gmail.com>
2024-10-26 13:36:46 +0200

    Nested Arrays

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -11,6 +11,23 @@ contract TicTacToe {
     */
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
+        // Horizontal & Vertical & Cross right
+        uint256 sumCrossRight = 0;
+        uint256 sumCrossLeft = 0;
+        for (uint256 i = 0; i < board.length; i++) {
+            uint256 sumRows = 0;
+            uint256 sumColumns = 0;
+
+            for (uint256 j = 0; j < board[i].length; j++) {
+                sumRows += board[i][j];
+                sumColumns += board[j][i];
+
+                if (i == j) sumCrossRight += board[i][j];
+                if (i + j == 3) sumCrossLeft += board[i][j];
+            }
+            if (sumRows == 3 || sumColumns == 3) return true;
+        }
+
+        return sumCrossRight == 3 || sumCrossLeft == 3;
     }
 }
