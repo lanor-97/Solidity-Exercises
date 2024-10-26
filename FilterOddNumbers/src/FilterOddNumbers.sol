6b83d419b4917258b3fd42d827da718b14df8e7c
lanor <giuseppe.trieste.97@gmail.com>
2024-10-26 12:52:24 +0200

    Introduction to arrays and strings

--- FilterOddNumbers/src/FilterOddNumbers.sol
+++ FilterOddNumbers/src/FilterOddNumbers.sol
@@ -10,11 +10,25 @@ contract FilterOddNumbers {
               you need to count the even numbers then declare an array of that size.
     */
 
-    function filterOdd(uint256[] memory _arr)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        // your code here
+    function filterOdd(uint256[] memory _arr) public pure returns (uint256[] memory) {
+        // Array with odd numbers removed = array with only even numbers
+        uint256 evenNumbersCounter = 0;
+        for (uint256 i = 0; i < _arr.length; i++) {
+            bool isEven = _arr[i] % 2 == 0;
+            if (isEven) evenNumbersCounter++;
+        }
+
+        // Declaring the returning array
+        uint256[] memory retVal = new uint256[](evenNumbersCounter);
+        uint256 currentIdx = 0;
+        for (uint256 i = 0; i < _arr.length; i++) {
+            bool isEven = _arr[i] % 2 == 0;
+            if (isEven) {
+                retVal[currentIdx] = _arr[i];
+                currentIdx++;
+            }
+        }
+
+        return retVal;
     }
 }
--- FizzBuzz/src/FizzBuzz.sol
+++ FizzBuzz/src/FizzBuzz.sol
@@ -7,5 +7,13 @@ contract FizzBuzz {
         // if n is divisible by 5, return "buzz"
         // if n is divisible be 3 and 5, return "fizz buzz"
         // otherwise, return an empty string
+
+        bool divisibleBy3 = n % 3 == 0;
+        bool divisibleBy5 = n % 5 == 0;
+
+        if (divisibleBy5 && divisibleBy3) return "fizz buzz";
+        if (divisibleBy3) return "fizz";
+        if (divisibleBy5) return "buzz";
+        return "";
     }
 }
